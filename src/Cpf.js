55998b304c483d684132d5473957fb76c5e036f7
Vinícius Albuquerque <vinicius.albuquerque@outlook.com>
2022-05-02 01:09:30 -0300

    Refactors CPF validate function

--- src/Cpf.js
+++ src/Cpf.js
@@ -1,46 +1,44 @@
-exports.validate = function (str) {
-	if (str !== null) {
-        if (str !== undefined) {
-            if (str.length >= 11 || str.length <= 14){
-                str=str
-                    .replace('.','')
-                    .replace('.','')
-                    .replace('-','')
-                    .replace(" ","");   
-                if (!str.split("").every(c => c === str[0])) {
-                    try{  
-                        let     d1, d2;  
-                        let     dg1, dg2, rest;  
-                        let     digito;  
-                            let     nDigResult;  
-                        d1 = d2 = 0;  
-                        dg1 = dg2 = rest = 0;  
-                        for (let nCount = 1; nCount < str.length -1; nCount++) {  
-                            // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
-                            // 	return false;
-                            // } else {
-                                digito = parseInt(str.substring(nCount -1, nCount));  							
-                                d1 = d1 + ( 11 - nCount ) * digito;  
-                                d2 = d2 + ( 12 - nCount ) * digito;  
-                            // }
-                        };  
-                        rest = (d1 % 11);  
-                        dg1 = (rest < 2) ? dg1 = 0 : 11 - rest;  
-                        d2 += 2 * dg1;  
-                        rest = (d2 % 11);  
-                        if (rest < 2)  
-                            dg2 = 0;  
-                        else  
-                            dg2 = 11 - rest;  
-                            let nDigVerific = str.substring(str.length-2, str.length);  
-                        nDigResult = "" + dg1 + "" + dg2;  
-                        return nDigVerific == nDigResult;
-                    }catch (e){  
-                        console.error("Erro !"+e);  
-                        return false;  
-                    }  
-                } else return false
-            }else return false;
-        }
-	} else return false;
+const FACTOR_DIGIT_1 = 10;
+const FACTOR_DIGIT_2 = 11;
+
+exports.validate = function (cpf) {
+  if (!cpf) return false;
+
+  const sanitizedCpf = sanitizeCpf(cpf);
+
+  if (!hasValidLength(sanitizedCpf)) return false;
+
+  if (repeatsAllNumbers(sanitizedCpf)) return false;
+
+  const digit1 = checkDigit(sanitizedCpf, FACTOR_DIGIT_1);
+  const digit2 = checkDigit(sanitizedCpf, FACTOR_DIGIT_2);
+  let checkingDigit = extractCheckingDigit(sanitizedCpf);
+  const calculatedDigit = `${digit1}${digit2}`;
+  return checkingDigit == calculatedDigit;
+};
+
+function repeatsAllNumbers(sanitizedCpf) {
+  const firstDigit = sanitizedCpf[0];
+  return [...sanitizedCpf].every((digit) => digit === firstDigit);
 }
+
+function hasValidLength(sanitizedCpf) {
+  return sanitizedCpf.length === 11;
+}
+
+function sanitizeCpf(cpf) {
+  return cpf.replace(/[.,\- ]/g, "");
+}
+
+function checkDigit(cpf, factor) {
+  let total = 0;
+  for (const digit of cpf) {
+    if (factor > 1) total += digit * factor--;
+  }
+  const rest = total % 11;
+  return rest < 2 ? 0 : 11 - rest;
+}
+
+const extractCheckingDigit = function (cpf) {
+  return cpf.slice(-2);
+};
